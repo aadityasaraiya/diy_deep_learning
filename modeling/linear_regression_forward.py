938864d21d5f4e80f69fc949adc0d60aa7bf98a5
aadityasaraiya <aadityasaraiya25@gmail.com>
2024-03-05 00:20:14 -0700

    Added linear regression forward pass

---
+++ modeling/linear_regression_forward.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numpy.typing import NDArray
+
+# Helpful functions:
+# https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
+# https://numpy.org/doc/stable/reference/generated/numpy.mean.html
+# https://numpy.org/doc/stable/reference/generated/numpy.square.html
+
+class Solution:
+    
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        # X is an Nx3 NumPy array
+        # weights is a 3x1 NumPy array
+        Y_hat = np.matmul(X, weights)
+        return np.round(Y_hat, 5)
+
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        # model_prediction is an Nx1 NumPy array
+        # ground_truth is an Nx1 NumPy array
+        mse_error = np.mean(np.square(model_prediction - ground_truth))
+        return round(mse_error, 5)
